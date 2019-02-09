2bb6ad1859d31342b77ff9e68f9f381207231ddd
Dekim12 <zykau_s@mail.ru>
2019-02-09 12:12:41 +0200

    Updates

---
+++ precache-manifest.5789d2667fc0d4f60c8a69a11a06c6c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad02569271f22ba58a3",
+    "url": "/mentor-dashboard/static/js/runtime~main.5ad02569.js"
+  },
+  {
+    "revision": "0829f0f5ef466b73d68d",
+    "url": "/mentor-dashboard/static/js/main.0829f0f5.chunk.js"
+  },
+  {
+    "revision": "a6c13ae4c8ba72e78c7b",
+    "url": "/mentor-dashboard/static/js/1.a6c13ae4.chunk.js"
+  },
+  {
+    "revision": "0829f0f5ef466b73d68d",
+    "url": "/mentor-dashboard/static/css/main.cda9599d.chunk.css"
+  },
+  {
+    "revision": "dca33e4ac3c743301d215c2686d59650",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
